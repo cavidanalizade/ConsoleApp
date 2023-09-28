b86e584724aa6dbfea67eef969a88fbc4c3e6cfc
cavidanalizade <145671273+cavidanalizade@users.noreply.github.com>
2023-09-28 11:23:42 +0400

    Add files via upload
    task 2

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 1; i <= n/2; i++)
+{
+    if (n % i == 0)
+    {
+        count++;
+    }
+}
+if (count == 1)
+{
+    Console.WriteLine( "sade ededdir");
+}
+else
+{
+    Console.WriteLine("Murekkeb ededdir");
+}
